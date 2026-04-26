b806731542bc5550213a45f2a06845a4c8a701c5
ygherri <mina.gh2060@gmail.com>
2026-04-26 22:09:05 +0200

    feat: add CORS for production

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const { errorHandler, notFound } = require("./middleware/errorHandler");
 const patientRoutes = require("./routes/patients");
 const appointmentRoutes = require("./routes/appointments");
 
 const app = express();
+// CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mediapp-web.netlify.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
