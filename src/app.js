9a3624869d5cdcb4218f9c8a8292e2a98f152744
ygherri <mina.gh2060@gmail.com>
2026-04-26 22:29:21 +0200

    fix cors origin

--- src/app.js
+++ src/app.js
@@ -8,14 +8,13 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://mediapp-web.netlify.app/",
-    ],
+    origin: ["http://localhost:5173", "https://mediapp-web.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }),
 );
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
